9f3accbdfe73a37add5eca314884ab9548cd5658
efeabogidi-pixel <efeabogidi@gmail.com>
2026-01-28 00:47:23 +1030

    Delete assets directory

--- assets/main.js
+++
@@ -1,7 +0,0 @@
-const reveals = document.querySelectorAll(".reveal");
-const io = new IntersectionObserver((entries)=>{
-  entries.forEach(e=>{
-    if(e.isIntersecting) e.target.classList.add("show");
-  });
-},{threshold:0.12});
-reveals.forEach(r=>io.observe(r));
